00b52fe289d7dc3ddb1def4e6e4cf842848b0847
cantepoler <canteropol@gmail.com>
2025-10-19 11:50:49 +0200

    UserGroups i autorització per diferents permisos implementat, falta testejar.

--- src/baseNoStates/Schedule.java
+++ src/baseNoStates/Schedule.java
@@ -1,20 +1,22 @@
 package baseNoStates;
+import java.lang.reflect.Array;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Schedule {
   // This class is meant to define the schedule when a userGroup is allowed to do actions,
   // also, it is used to make sure if a certain LocalDateTime is inside it.
-  private Set<DayOfWeek> weekDays;
+  private ArrayList<DayOfWeek> weekDays;
   private LocalTime startTime;
   private LocalTime endTime;
   private LocalDate startDate;
   private LocalDate endDate;
 
-  public Schedule(Set<DayOfWeek> weekDays, LocalTime startTime, LocalTime endTime,
+  public Schedule(ArrayList<DayOfWeek> weekDays, LocalTime startTime, LocalTime endTime,
                   LocalDate startDate, LocalDate endDate) {
     this.weekDays = weekDays;
     this.startTime = startTime;
--- src/baseNoStates/User.java
+++ src/baseNoStates/User.java
@@ -1,15 +1,18 @@
 package baseNoStates;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class User {
   private final String name;
   private final String credential;
-  private ArrayList<Area> areas;
+  private UserGroup userGroup;
+  private ArrayList<String> areas;
 
-  public User(String name, String credential) {
+  public User(String name, String credential, UserGroup userGroup) {
     this.name = name;
     this.credential = credential;
+    this.userGroup = userGroup;
   }
 
   public String getCredential() {
@@ -21,17 +24,16 @@ public class User {
     return "User{name=" + name + ", credential=" + credential + "}";
   }
 
-  //Giving a space, it covers all the areas to which it has access
-  //and returns if you have access to that site or not
   public boolean canBeInSpace(Space space) {
-    for(Area area : areas) {
-      for (Space space1 : area.getSpaces()) {
-        if (space1 == space) {
-          return true;
-        }
-      }
-    }
-    return false;
+    return userGroup.canBeInSpace(space);
+  }
+
+  public boolean canSendRequests(LocalDateTime dateTime) {
+    return userGroup.canSendRequests(dateTime);
+  }
+
+  public boolean canDoAction(String action) {
+    return userGroup.canDoAction(action);
   }
 
   private ArrayList<Space> getSpaces() {
